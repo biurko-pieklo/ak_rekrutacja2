4898e9e0f9d137cd28b3f37ed427c283b72e77be
Matthew Cherry <matiwisnia16@gmail.com>
2023-12-21 20:56:33 +0100

    Add User active field

--- src/Core/User/Domain/User.php
+++ src/Core/User/Domain/User.php
@@ -25,14 +25,32 @@ class User
      */
     private string $email;
 
+    /**
+     * @ORM\Column(type="boolean", nullable=false)
+     */
+    private ?bool $active;
+
     public function __construct(string $email)
     {
         $this->id = null;
         $this->email = $email;
+        $this->active = null;
     }
 
     public function getEmail(): string
     {
         return $this->email;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): static
+    {
+        $this->active = $active;
+
+        return $this;
     }
 }
