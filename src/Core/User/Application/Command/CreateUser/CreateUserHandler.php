102e1dececb13b6f051a28c06f76ecc622253c02
Matthew Cherry <matiwisnia16@gmail.com>
2023-12-26 19:09:22 +0100

    Send email on user created

--- src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
+++ src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
@@ -2,6 +2,8 @@
 
 namespace App\Core\User\Application\Command\CreateUser;
 
+use App\Common\Mailer\SMPTMailer;
+use App\Core\User\Domain\Exception\UserNotFoundException;
 use App\Core\User\Domain\User;
 use App\Core\User\Domain\Repository\UserRepositoryInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -15,11 +17,28 @@ class CreateUserHandler
 
     public function __invoke(CreateUserCommand $command): void
     {
-        $this->userRepository->save(new User(
+        $user = new User(
             $command->email,
             false,
-        ));
+        );
+
+        $this->userRepository->save($user);
 
         $this->userRepository->flush();
+
+        try {
+            if ($this->userRepository->getByEmail($user->getEmail())) {
+
+                $mailer = new SMPTMailer();
+
+                $mailer->send(
+                    $user->getEmail(),
+                    'Utworzono konto',
+                    'Zarejestrowano konto w systemie. Aktywacja konta trwa do 24h',
+                );
+            }
+        } catch (UserNotFoundException $e) {
+            echo $e->getMessage();
+        }
     }
 }
